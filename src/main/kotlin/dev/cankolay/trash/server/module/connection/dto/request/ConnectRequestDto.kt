281c1caa2542af0953621abf5de80d5a9cc15664
Can Kolay <budchirp@gmail.com>
2026-05-26 21:42:52 +0300

    chore: small fix

--- src/main/kotlin/dev/cankolay/trash/server/module/connection/dto/request/ConnectRequestDto.kt
+++ src/main/kotlin/dev/cankolay/trash/server/module/connection/dto/request/ConnectRequestDto.kt
@@ -1,9 +1,11 @@
 package dev.cankolay.trash.server.module.connection.dto.request
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotEmpty
 
 data class ConnectRequestDto(
+    @param:JsonProperty("applicationId")
     @field:NotBlank
     val applicationId: String,
 
